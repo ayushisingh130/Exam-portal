affd355108c3fb28f2d04b3bb6021db9d8d4eb7a
Richa Singh <ayushisingh0713@gmail.com>
2021-11-02 15:33:18 +0530

    update getCandidate

--- src/Components/GetCandidate/GetCandidate.js
+++ src/Components/GetCandidate/GetCandidate.js
@@ -7,7 +7,7 @@ const GetCandidate = () => {
 
     useEffect(() => {
         Axios.get(" https://radiant-scrubland-91561.herokuapp.com/api/candidates/all")
-        .then(response.map((candidate)=>{console.log(candidate.data)} ))
+        .then(response => console.log(response.data))
         .then(error => console.log(error))
        
     },[]);
@@ -19,15 +19,18 @@ const GetCandidate = () => {
             <h1>GetCandidate</h1>
             <button >get</button>
             <div>
-            { users.map(user => (
-                <div>
-                <h1>{user.id}</h1>
-                <h2>hey</h2>
-                <h1>{user.name}</h1>
-
-                </div>
+            { users.map((user) => 
                 
-            ))}
+                <>
+                    <div>
+                    <h1>{user.id}</h1>
+                    <h2>hey</h2>
+                    <h1>{user.name}</h1>
+
+                    </div>
+                </>
+                
+            )}
             </div>
         </>
     );
